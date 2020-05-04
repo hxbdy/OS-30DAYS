99f6e850b1fe83bd4197ebe535bfeb355b80ab75
hxbdy <hxbdy541@gmail.com>
2020-05-05 02:19:51 +0900

    13_day harib10i

--- tolset/emu84/timer.c
+++ tolset/emu84/timer.c
@@ -10,15 +10,20 @@ struct TIMERCTL timerctl;
 
 void init_pit(void){
     int i;
+    struct TIMER *t;
     io_out8(PIT_CTRL, 0x34);
     io_out8(PIT_CNT0, 0x9c);
     io_out8(PIT_CNT0, 0x2e);
     timerctl.count = 0;
-    timerctl.next = 0xffffffff;
-    timerctl.using = 0;
     for(i = 0; i < MAX_TIMER; i++){
         timerctl.timers0[i].flags = 0;
     }
+    t = timer_alloc();
+    t->timeout = 0xffffffff;
+    t->flags = TIMER_FLAGS_USING;
+    t->next = 0;
+    timerctl.t0 = t;
+    timerctl.next = 0xffffffff;
     return;
 }
 
@@ -38,59 +43,64 @@ void timer_free(struct TIMER *timer){
     return;
 }
 
-void timer_init(struct TIMER *timer, struct FIFO8 *fifo, unsigned char data){
+void timer_init(struct TIMER *timer, struct FIFO32 *fifo, int data){
     timer->fifo = fifo;
     timer->data = data;
     return;
 }
 
 void timer_settime(struct TIMER *timer, unsigned int timeout){
-	int e, i, j;
+	int e;
+    struct TIMER *t, *s;
 	timer->timeout = timeout + timerctl.count;
 	timer->flags = TIMER_FLAGS_USING;
 	e = io_load_eflags();
 	io_cli();
+    t = timerctl.t0;
+    if(timer->timeout <= t->timeout){
+        timerctl.t0 = timer;
+        timer->next = t;
+        timerctl.next = timer->timeout;
+        io_store_eflags(e);
+        return;
+    }
+    if(timer->timeout <= t->timeout){
+        timerctl.t0 = timer;
+        timer->next = t;
+        timerctl.next = timer->timeout;
+        io_store_eflags(e);
+        return;
+    }
 	/* ‚Ç‚±‚É“ü‚ê‚ê‚Î‚¢‚¢‚©‚ð’T‚· */
-	for (i = 0; i < timerctl.using; i++) {
-		if (timerctl.timers[i]->timeout >= timer->timeout) {
-			break;
+	for (;;) {
+        s = t;
+        t = t->next;
+		if (timer->timeout <= t->timeout) {
+			s->next = timer;
+            timer ->next = t;
+            io_store_eflags(e);
+            return;
 		}
 	}
-	/* ‚¤‚µ‚ë‚ð‚¸‚ç‚· */
-	for (j = timerctl.using; j > i; j--) {
-		timerctl.timers[j] = timerctl.timers[j - 1];
-	}
-	timerctl.using++;
-	/* ‚ ‚¢‚½‚·‚«‚Ü‚É“ü‚ê‚é */
-	timerctl.timers[i] = timer;
-	timerctl.next = timerctl.timers[0]->timeout;
-	io_store_eflags(e);
-	return;
 }
 
 void inthandler20(int *esp){
-    int i, j;
+    struct TIMER *timer;
     io_out8(PIC0_OCW2, 0x60);
     timerctl.count++;
     if(timerctl.next > timerctl.count){
         return;
     }
-    for(i = 0; i < timerctl.using; i++){
-        if(timerctl.timers[i]->timeout > timerctl.count){
+    timer = timerctl.t0;
+    for(;;){
+        if(timer->timeout > timerctl.count){
             break;
         }
-        timerctl.timers[i]->flags = TIMER_FLAGS_ALLOC;
-        fifo8_put(timerctl.timers[i]->fifo, timerctl.timers[i]->data);
+        timer->flags = TIMER_FLAGS_ALLOC;
+        fifo32_put(timer->fifo, timer->data);
+        timer = timer->next;
     }
-    timerctl.using -= i;
-    for(j = 0; j<timerctl.using; j++){
-        timerctl.timers[j] = timerctl.timers[i+j];
-    }
-    if(timerctl.using > 0){
-        timerctl.next = timerctl.timers[0]->timeout;
-    }
-    else{
-        timerctl.next = 0xffffffff;
-    }
+    timerctl.t0 = timer;
+    timerctl.next = timer->timeout;
     return;
 }
