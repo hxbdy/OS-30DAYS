f346a44811e9f816fd606890e9f43783c5e28029
hxbdy <hxbdy541@gmail.com>
2020-05-08 10:27:23 +0900

    15_day harib12b

--- tolset/emu84/bootpack.c
+++ tolset/emu84/bootpack.c
@@ -6,6 +6,15 @@ void make_window8(unsigned char *buf, int xsize, int ysize, char *title);
 void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b, char *s, int l);
 void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c);
 
+struct TSS32{
+	int backlink, esp0, ss0, esp1, ss1, esp2, ss2, cr3;
+	int eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi;
+	int es, cs, ss, ds, fs, gs;
+	int ldtr, iomap;
+};
+
+void task_b_main(void);
+
 void HariMain(void){
 
     struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
@@ -13,7 +22,7 @@ void HariMain(void){
 	char s[40];
 	int fifobuf[128];
 	struct TIMER *timer, *timer2, *timer3;
-    int mx, my, i, cursor_x, cursor_c;
+    int mx, my, i, cursor_x, cursor_c, task_b_esp;
     unsigned int memtotal;
     struct MOUSE_DEC mdec;
     struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
@@ -28,6 +37,9 @@ void HariMain(void){
 		0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
 		'2', '3', '0', '.'
 	};
+
+	struct TSS32 tss_a, tss_b;
+	struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *)ADR_GDT;	
 
     init_gdtidt();
     init_pic();
@@ -85,6 +97,31 @@ void HariMain(void){
     sprintf(s, "memory %dMB   free : %dKB",
 			memtotal / (1024 * 1024), memman_total(memman) / 1024);
 	putfonts8_asc_sht(sht_back, 0, 32, COL8_FFFFFF, COL8_008484, s, 40);
+
+	tss_a.ldtr = 0;
+	tss_a.iomap = 0x40000000;
+	tss_b.ldtr = 0;
+	tss_b.iomap = 0x40000000;
+	set_segmdesc(gdt + 3, 103, (int) &tss_a, AR_TSS32);
+	set_segmdesc(gdt + 4, 103, (int) &tss_b, AR_TSS32);
+	load_tr(3 * 8);
+	task_b_esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024;
+	tss_b.eip = (int) &task_b_main;
+	tss_b.eflags = 0x00000202; /* IF = 1; */
+	tss_b.eax = 0;
+	tss_b.ecx = 0;
+	tss_b.edx = 0;
+	tss_b.ebx = 0;
+	tss_b.esp = task_b_esp;
+	tss_b.ebp = 0;
+	tss_b.esi = 0;
+	tss_b.edi = 0;
+	tss_b.es = 1 * 8;
+	tss_b.cs = 2 * 8;
+	tss_b.ss = 1 * 8;
+	tss_b.ds = 1 * 8;
+	tss_b.fs = 1 * 8;
+	tss_b.gs = 1 * 8;
 
     for(;;){
         io_cli();
@@ -151,6 +188,7 @@ void HariMain(void){
 			}
 			else if(i == 10){
 				putfonts8_asc_sht(sht_back, 0, 64, COL8_FFFFFF, COL8_008484, "10[sec]", 7);
+				taskswitch4();
 			}
 			else if(i == 3){
 				putfonts8_asc_sht(sht_back, 0, 80, COL8_FFFFFF, COL8_008484, "3[sec]", 6);			
@@ -239,4 +277,30 @@ void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c){
 	boxfill8(sht->buf, sht->bxsize, COL8_C6C6C6, x1 + 1, y0 - 2, x1 + 1, y1 + 1);
 	boxfill8(sht->buf, sht->bxsize, c,           x0 - 1, y0 - 1, x1 + 0, y1 + 0);
 	return;
+}
+
+void task_b_main(void){
+	struct FIFO32 fifo;
+	struct TIMER *timer;
+	int i, fifobuf[128];
+
+	fifo32_init(&fifo, 128, fifobuf);
+	timer = timer_alloc();
+	timer_init(timer, &fifo, 1);
+	timer_settime(timer, 500);
+
+	for(;;){
+		io_cli();
+		if(fifo32_status(&fifo) == 0){
+			io_sti();
+			io_hlt();
+		}
+		else{
+			i = fifo32_get(&fifo);
+			io_sti();
+			if(i == 1){
+				taskswitch3();
+			}
+		}
+	}
 }
--- tolset/emu84/naskfunc.nas
+++ tolset/emu84/naskfunc.nas
@@ -9,9 +9,11 @@
     GLOBAL    _io_load_eflags, _io_store_eflags
     GLOBAL	  _load_gdtr, _load_idtr
 	GLOBAL	_load_cr0, _store_cr0
+	GLOBAL  _load_tr
 	GLOBAL  _asm_inthandler20, _asm_inthandler21
     GLOBAL	_asm_inthandler27, _asm_inthandler2c
 	GLOBAL	_memtest_sub
+	GLOBAL  _taskswitch3, _taskswitch4
 	EXTERN  _inthandler20, _inthandler21
 	EXTERN	_inthandler27, _inthandler2c
 
@@ -98,6 +100,10 @@ _load_cr0:
 _store_cr0:
 		MOV		EAX,[ESP+4]
 		MOV		CR0,EAX
+		RET
+
+_load_tr:		; void load_tr(int tr);
+		LTR		[ESP+4]			; tr
 		RET
 
 _asm_inthandler20:
@@ -195,4 +201,12 @@ mts_fin:
 		POP		EBX
 		POP		ESI
 		POP		EDI
+		RET
+
+_taskswitch3:	; void taskswitch3(void);
+		JMP		3*8:0
+		RET
+
+_taskswitch4:	; void taskswitch4(void);
+		JMP		4*8:0
 		RET
